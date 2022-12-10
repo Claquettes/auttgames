ee78dd690da72b57edd3530f8c136fa4502f6e67
FlashOnFire_ <flashonfire.dev@gmail.com>
2022-12-11 00:59:16 +0100

    Fix mimir

--- public/js/mimir.js
+++ public/js/mimir.js
@@ -1,6 +1,5 @@
-const socket = io('/mimir');
 // This is an array of URLs to the GIFs that you want to display
-var gifs = [
+let gifs = [
     "https://media.tenor.com/7jg0Rc46pfoAAAAC/a-mimir.gif",
     "https://media.tenor.com/W7oQekS6RS8AAAAd/choque-a-mimir-a-mimir.gif",
     "https://media.giphy.com/media/DxlzS6VnwbJm2S8h94/giphy.gif"
@@ -8,15 +7,16 @@ var gifs = [
 
 // This function will randomly select a GIF from the array and display it on the page
 function showRandomGif() {
-    var id = Math.floor(Math.random() * gifs.length);
-    var randomGif = gifs[id];
+    let id = Math.floor(Math.random() * gifs.length);
+    let randomGif = gifs[id];
 
-    var img = document.createElement("img");
+    let img = document.createElement("img");
     img.src = randomGif;
 
-    var gifContainer = document.getElementById("gif-container");
+    let gifContainer = document.getElementById("gif-container");
     gifContainer.innerHTML = "";
 
     gifContainer.appendChild(img);
-    }
+}
+
 showRandomGif();
