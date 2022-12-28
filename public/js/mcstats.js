3d7b3eead79b21dd93875638880cdfe2243384a4
Claquettes <math@claq.fr>
2022-12-28 22:46:36 +0100

    burgir

--- public/js/mcstats.js
+++ public/js/mcstats.js
@@ -1,38 +1,57 @@
-//execute the function to get the data using ocr, when the user click on the sendReceipt button
-function sendReceipt() {
-    //get the image
-    var img = document.getElementById("receipt");
-    //get the image source
-    var imgSrc = img.src;
-    //get the data using ocr
-    var data = ocr(imgSrc);
-    //send the data to the server
-    $.ajax({
-        url: '/sendReceipt',
-        type: 'POST',
-        data: {data: data},
-        success: function (data) {
-            console.log(data);
-        }
-    });
-}
-const ctx = document.getElementById('myChart');
-  
-new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
+
+let data = document.currentScript.getAttribute("data");
+let bestOf = 0; 
+let maxiBestOf = 0;
+let menuMcFirst = 0;
+let maxiMcFirst = 0;
+let happymeal = 0;
+console.log(data);
+for (let order in data){
+  console.log(order);
+  let orderdata = JSON.parse(order.order_json);
+  for(let menu in orderdata.menus){
+    switch(menu.menu_id){
+      case 1: 
+        bestOf++;
+        break;
+      case 2:
+        maxiBestOf++;
+        break;
+      case 3:
+        menuMcFirst++;
+        break;
+      case 4:
+        maxiMcFirst++;
+        break;
+      case 5:
+        happymeal++;
+        break;
   }
+}}
+
+
+const chartData = {
+  menu_id: 1,
+  products: [
+    { name: "BestOf", value: bestOf},
+    { name: "maxiBestOf", value: maxiBestOf },
+    { name: "McFirst", value: menuMcFirst },
+    { name: "Maxi McFirst", value: maxiMcFirst },
+    { name: "Happymeal", value: happymeal },
+  ]
+};
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: chartData.products.map(product => product.name),
+    datasets: [
+      {
+        label: `Menu ${chartData.menu_id}`,
+        data: chartData.products.map(product => product.value),
+      },
+    ],
+  },
 });
+console.log(document.currentScript.getAttribute("data"));
+
