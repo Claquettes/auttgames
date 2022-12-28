e77687e5a879543fbc7abfecd6c3366a4a103dc1
Claquettes <math@claq.fr>
2022-12-28 22:46:26 +0100

    burger

--- mcstats.js
+++ mcstats.js
@@ -1,10 +1,22 @@
+const db = require("./db");
 const path = require("path");
+const { checkAuthenticated } = require("./auth");
 
 function init(app) {
-    app.get('/mcstats', (req, res) => {
-       res.render("mcstats/mcstats")
+    app.get('/mcstats',(req, res) => { //todo: checkAuthenticated
+        db.getUserOrders(1).then((result) => {  //changer 1 par req.user.id
+            res.render("mcstats/mcstats", { data: result })
+            //console.dir(result);
+            //console.dir(result[0].order_json);
+            //console.dir(result[0].order_json.menus[0].products);
+            
+        }).catch((err) => {
+            res.sendStatus(500);
+            console.log(err)
+        });
     });
 }
+
 
 module.exports = {
     init
