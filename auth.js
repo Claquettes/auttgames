b476102cbef68719b938e9e274d52cd3f74bc371
Claquettes <math@claq.fr>
2023-05-24 13:40:31 +0200

    deambu

--- auth.js
+++ auth.js
@@ -1,66 +1,112 @@
-require('dotenv').config()
+const flash = require('express-flash')
+const methodOverride = require('method-override')
+const passport = require('passport')
 
-const express = require('express');
+const sessionController = require('./sessionController')
+const {sanitize} = require("./passport-config");
 
-const app = express();
-const http = require('http').createServer(app);
-const path = require('path');
-const mysql = require('mysql2');
-const db = require('./db')
-const auth = require('./auth')
+function init(app, db, session_secret) {
+    app.use(flash())
+    app.use(sessionController.sessionMiddleware)
 
-const port = 80;
+    require('./passport-config').initialize(passport, db)
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+    app.use(passport.initialize())
+    app.use(passport.session(
+        {
+            secret: session_secret,
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+                sameSite: true,
+                secure: true
+            },
+        }
+    ))
+    app.use(methodOverride('_method'))
 
-db.init(mysql, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD);
-auth.init(app, db, process.env.SESSION_SECRET)
+    app.get('/profile', checkAuthenticated, (req, res) => {
+        db.getStats(req.user.id).then((stats) => {
+            res.render('profile',
+                {
+                    username: req.user.username,
+                    avatar: req.user.avatar,
+                    banner: req.user.banner,
+                    stats: stats
+                })
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(500)
+        })
+    })
 
-    app.get('/mcstats', checkAuthenticated, (req, res) => { //on autorise l'accès seulement 
-        //on recupère l'id du compte
-        let id = req.user.id;
-        if (req.user.id !==4) {
-            res.sendStatus(403);
-          } else {
-            res.render("mcstats/mcstats.ejs");
-          }
+    
+
+    app.get('/login', checkNotAuthenticated, (req, res) => {
+        res.render('login')
+    })
+
+    app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+            successRedirect: '/profile',
+            failureRedirect: '/login',
+            failureFlash: true
+        })
+    )
+    app.get('/register', checkNotAuthenticated, (req, res) => {
+        res.render('register')
+    })
+
+    app.post('/register', checkNotAuthenticated, async (req, res) => {
+        if (req.body.username.trim() === '' || req.body.password.trim() === '') {
+            res.render('/register', {message: "Nom d'utilisateur ou mot de passe vide"})
+        }
+
+        db.registerUser(sanitize(req.body.username), sanitize(req.body.password), (req.headers['x-forwarded-for'] || req.socket.remoteAddress), (err, success, message) => {
+            if (err) {
+                console.log(err)
+                res.sendStatus(500)
+            } else {
+                if (success) {
+                    res.render('login', {message: message})
+                } else {
+                    res.render('register', {message: message})
+                }
+            }
+        })
+    })
+
+    app.delete('/logout', (req, res, next) => {
+        req.logOut((err) => {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/login');
+        });
+
     });
 
+    app.get('/settings', checkAuthenticated, (req, res) => {
+        res.render('settings', {user: req.user});
+    })
 
+}
 
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/profile')
+    }
+    next()
+}
 
-const socketio = require('socket.io')(http, {'pingInterval': 3000, 'pingTimeout': 5000});
-
-app.use('/AUTT', express.static(path.join(__dirname, 'AUTT')));
-app.use('/2048', express.static(path.join(__dirname, '2048')));
-//perso claquettes
-app.use('/casse-briques', express.static(path.join(__dirname, 'casse-briques')));
-app.use('/polyressources', express.static(path.join(__dirname, 'polyressources')));
-app.use('/snak', express.static(path.join(__dirname, 'snak')));
-app.use('/garden', express.static(path.join(__dirname, 'garden')));
-app.use('/host', express.static(path.join(__dirname, 'host')));
-
-
-
-
-app.get('/2048', (req, res) => {
-    res.render('2048/2048');
-});
-
-require('./dinautt').init(app, socketio.of('/dinautt'));
-require('./citations').init(app, socketio.of('/citations'));
-require('./morpion').init(app, socketio.of('/morpion'));
-require('./envoie').init(app);
-require('./mimir').init(app);
-require('./mcstats').init(app);
-require('./hidden').init(app);
-
-
-http.listen(port, () => {
-    console.log(`Letsgo ca marche, tu peux test ici http://localhost:${port}/`);
-});
+module.exports = {
+    init: init,
+    checkAuthenticated: checkAuthenticated,
+}
