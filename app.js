72023592523aaef425759bbff4fb2216fcfeab09
Claquettes <math@claq.fr>
2023-05-24 09:56:17 +0200

    music

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
-
 const app = express();
 const http = require('http').createServer(app);
 const path = require('path');
 const mysql = require('mysql2');
-const db = require('./db')
-const auth = require('./auth')
+const db = require('./db');
+const auth = require('./auth');
+const fs = require('fs');
+const mm = require('music-metadata');
 
 const port = 80;
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 db.init(mysql, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD);
-auth.init(app, db, process.env.SESSION_SECRET)
+auth.init(app, db, process.env.SESSION_SECRET);
 
 app.use('/bootstrap/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/bootstrap/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/', express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
-const socketio = require('socket.io')(http, {'pingInterval': 3000, 'pingTimeout': 5000});
+const socketio = require('socket.io')(http, { 'pingInterval': 3000, 'pingTimeout': 5000 });
 
 app.use('/AUTT', express.static(path.join(__dirname, 'AUTT')));
 app.use('/2048', express.static(path.join(__dirname, '2048')));
@@ -37,11 +34,8 @@ app.use('/snak', express.static(path.join(__dirname, 'snak')));
 app.use('/garden', express.static(path.join(__dirname, 'garden')));
 app.use('/host', express.static(path.join(__dirname, 'host')));
 
-
-
-
 app.get('/2048', (req, res) => {
-    res.render('2048/2048');
+  res.render('2048/2048');
 });
 
 require('./dinautt').init(app, socketio.of('/dinautt'));
@@ -51,20 +45,43 @@ require('./envoie').init(app);
 require('./mimir').init(app);
 require('./mcstats').init(app);
 
-/*
+// Add the code for the music player routes below
 
-db.getUserOrders(1).then((result) => {
-    for (let i = 0; i < result.length; i++) {
-        result[i].order_json = JSON.parse(result[i].order_json);
-    }
+const songsDirectory = path.join(__dirname, 'mcstats/my_music');
 
-    let commande = result[0].order_json
-
-    console.dir(result);
-    console.dir(result[0].order_json);
-    console.dir(result[0].order_json.menus[0].products);
+app.get('/', async (req, res) => {
+  const songs = await getSongs();
+  res.render('index', { songs });
 });
-*/
+
+app.use('/my_music', express.static(songsDirectory));
+
+async function getSongs() {
+  const files = await fs.promises.readdir(songsDirectory);
+  const songs = [];
+
+  for (const file of files) {
+    if (file.endsWith('.mp3')) {
+      const path = `/my_music/${file}`;
+      const metadata = await mm.parseFile(path);
+      const title = metadata.common.title || file.slice(0, -4);
+      const duration = formatTime(metadata.format.duration);
+      const artist = metadata.common.artist || 'Artist Name';
+
+      songs.push({ title, duration, artist, path });
+    }
+  }
+
+  return songs;
+}
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  const formattedTime = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  return formattedTime;
+}
+
 http.listen(port, () => {
-    console.log(`Letsgo ca marche, tu peux test ici http://localhost:${port}/`);
+  console.log(`Letsgo ca marche, tu peux test ici http://localhost:${port}/`);
 });
